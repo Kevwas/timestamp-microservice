af03ed7b7249a38cd6cfc301b2ed08d0f80c0379
kevwas <arizakevin.dev@gmail.com>
2022-03-21 02:30:12 -0500

    Passed FCC Tests

--- index.js
+++ index.js
@@ -2,46 +2,54 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const port = process.env.PORT || 3000;
+
+// ENABLE CORS FOR TESTING PURPOSES
+app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // ENABLE USE OF STATIC FILES
 app.use("/public", express.static(__dirname + "/public"));
 
-// MIDDLEWARE FOR ALL REQUESTS
+// LOG REQUEST INFORMATION
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
-// SENDFILE METHOD WITH __dirname
+// HOME PAGE ROUTING
 app.get("/", (req, res) => {
   const absolutePath = __dirname + "/views/index.html";
   res.sendFile(absolutePath);
 });
 
+// RETURNS CURRENT TIMESTAMP IF date_string IS EMPTY
 app.get("/api/", (req, res) => {
   const event = new Date();
   res.json({
-    unix: event.valueOf(),
+    unix: event.getTime(),
     utc: event.toUTCString(),
   });
 });
 
-app.get("/api/:date", (req, res) => {
-  let { date } = req.params;
+// RETURNS JSON OBJECT WITH UNIX AND UTC KEYS
+app.get("/api/:date_string", (req, res) => {
+  let { date_string } = req.params;
 
-  if (!isNaN(Number(date))) {
-    date = parseFloat(date);
+  if (!isNaN(Number(date_string))) {
+    date_string = parseFloat(date_string);
   }
 
-  const event = new Date(date);
-
-  if (event.toUTCString() === "Invalid Date") {
+  if (new Date(date_string).toString() === "Invalid Date") {
     res.json({ error: "Invalid Date" });
+    return;
   }
+
+  const event = new Date(date_string);
+  console.log(event);
 
   res.json({
     unix: event.valueOf(),
@@ -49,6 +57,8 @@ app.get("/api/:date", (req, res) => {
   });
 });
 
-app.listen(port, function () {
-  console.log("timestamp-microservice is listening on port " + port + "...");
+const listener = app.listen(process.env.PORT, function () {
+  console.log(
+    "timestamp-microservice is listening on port " + listener.address().port
+  );
 });
